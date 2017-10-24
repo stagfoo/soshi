b41f30d7ec28c1e9009f99d2ad5a842c10726876
Alex King <alexanderddking@gmail.com>
2017-10-25 07:33:52 +0900

    yoyo in react

---
+++ CHANGELOG.md
@@ -0,0 +1,38 @@
+
+# CHANGELOG 0.1.2 
+
+---
+### Build: example-soshi 
+
+### Date: 2017-10-25 
+
+---
+### Commits 
+
+
+
+
+# CHANGELOG 0.1.3 
+
+---
+### Build: example-soshi 
+
+### Date: 2017-10-25 
+
+---
+### Commits 
+
+
+
+
+# CHANGELOG 0.1.4 
+
+---
+### Build: example-soshi 
+
+### Date: 2017-10-25 
+
+---
+### Commits 
+
+
--- src/render.js
+++ src/render.js
@@ -2,34 +2,29 @@
 // import Slider from 'components/slider';
 // import helloWorld from 'components/list';
 import Counter from 'components/counter';
-import list from 'components/yoyolist';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+//import list from 'components/yoyolist';
+
 // const homeSlider = new Slider(document.body);
 // const img = document.createElement('img')
 //  homeSlider.render([
 //    {name: 'beach3', content: `<img src='test' />` }
 //  ]);
+
 //
 // const List = require('./components/basic-yoyo.js');
-const ListEl = list(['grizzly', 'polar', 'brown']);
+// const ListEl = list(['grizzly', 'polar', 'brown']);
+
 const counterEl = new Counter().render();
+class CounterEl extends Component {
+  render() {
+    return counterEl;
+  }
+}
+ReactDOM.render(CounterEl, document.body);
+
 // document.body.appendChild(helloWorld);
 document.body.appendChild(counterEl);
-document.body.appendChild(ListEl);
-
-counterEl.addEventListener(
-  'updated',
-  e => {
-    console.log(e, 'update');
-    // e.target matches elem
-  },
-  false
-);
-
-counterEl.addEventListener(
-  'add_new',
-  e => {
-    console.log(e, 'add_new');
-    // e.target matches elem
-  },
-  false
-);
+//document.body.appendChild(ListEl);
