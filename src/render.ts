1e605bd64be501019668b28d2550a7bbf553d44d
Alex King <alexanderddking@gmail.com>
2017-12-15 07:42:42 +0900

    React working again

--- src/render.ts
+++ src/render.ts
@@ -3,5 +3,5 @@ import 'mdn-polyfills/Function.prototype.bind'; // 427 bytes
 import Showcase from 'components/showcase';
 import ReactShowcase from './react/showcase';
 
-//ReactShowcase();
+ReactShowcase();
 Showcase();
