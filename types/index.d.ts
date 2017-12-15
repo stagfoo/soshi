e43aafaea57c64610dd655da1d7b12c0102c634b
Alex King <stagfoo@users.noreply.github.com>
2017-12-15 09:09:15 +0900

    Update index.d.ts

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,9 +1,7 @@
   export interface Title {
-  el: HTMLElement;
-  update: (Object) => Boolean;
-  render: (Object) => HTMLElement;
-  template: () => HTMLElement;
-  props?: Object;
+    el: HTMLElement;
+    update: (Object) => Boolean;
+    render: (Object) => HTMLElement;
+    template: () => HTMLElement;
+    props?: Object;
   }
-
-export = Title; // this exports a function that returns objects of type Emitter, and a type called Emitter
